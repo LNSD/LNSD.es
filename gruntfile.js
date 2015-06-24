3e7a5e4545022012db298bdcdd76379283913161
LNSD <lorenzodelgado.dev@gmail.com>
2015-06-24 17:10:44 +0200

    Changed wercker box/container. Added responsive_image task to grunt. Minor style and script changes.

--- gruntfile.js
+++ gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
 			}
 		},
 
-		// Grunt tasks
+		// Grunt build tasks
 		clean: ['<%= dirs.output %>'],
 		copy: {
 			assets: { src: ['assets/*', '!assets/favicon.ico'], dest:'<%= dirs.output %>/assets/', expand: true, flatten: true },
@@ -25,13 +25,19 @@ module.exports = function(grunt) {
 		jade: {
 			build:{
 				options: { pretty: true },
-				files: { '<%= dirs.output %>/index.html': 'jade/index.jade' }
+				files: {
+                    '<%= dirs.output %>/index.html': 'jade/index.jade',
+                    '<%= dirs.output %>/404.html': 'jade/404.jade'
+                }
 			}
 		},
 		sass: {
 			build:{
 				options: { style: 'expanded' },
-				files: { '<%= dirs.output %>/styles/main.css': 'sass/*.scss' }
+				files: {
+                    '<%= dirs.output %>/styles/main.css': 'sass/main.scss',
+                    '<%= dirs.output %>/styles/404.css': 'sass/404.scss'
+                }
 			}
 		},
 		coffee: {
@@ -48,8 +54,22 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-jade');
-	grunt.loadNpmTasks('grunt-contrib-coffee');
-	grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.loadNpmTasks('grunt-sass');
+
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.config('responsive_images', {
+        options: {
+            engine:'im',
+            sizes: [
+                { name: 'xs', width: 480 },
+                { name: 'sm', width: 768 },
+                { name: 'md', width: 992 },
+                { name: "lg", width: 1200 }
+            ]
+        },
+        assets: { src: 'assets/*.{gif,png,jpg,jpeg}', dest:'<%= dirs.output %>/assets/', expand: true, flatten: true }
+    });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.config('watch', {
@@ -70,6 +90,6 @@ module.exports = function(grunt) {
         }
     });
 
-	grunt.registerTask('build', 'Compiles all of the source files and copies the assets to the build directory.', ['clean', 'copy', 'coffee', 'sass', 'jade']);
+	grunt.registerTask('build', 'Compiles all of the source files and copies the assets to the build directory.', ['clean', 'copy', 'responsive_images', 'coffee', 'sass', 'jade']);
 	grunt.registerTask('default', 'Compiles everything and copies it to build directory.', ['build']);
 };
--- scripts/main.coffee
+++ scripts/main.coffee
@@ -1,11 +1,2 @@
-# jQuery for page scrolling feature - requires jQuery Easing plugin
-
-$('a[href^="#"]').on 'click', (event) ->
-	target = $($(this).attr('href'))
-	if target.length
-		event.preventDefault()
-		$('html, body').animate { scrollTop: target.offset().top }, 1000
-
 # Asynchronously load the svg logo
-
-$('#main-logo').load 'assets/lnsd-big-logo.svg'
+$('#main-logo').load 'assets/big-logo.svg'
