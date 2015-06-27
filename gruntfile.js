44918ad10474d18fa55addf71798d2dd07bcd127
LNSD <lorenzodelgado.dev@gmail.com>
2015-06-27 14:53:33 +0200

    Sitemap generation added.

--- gruntfile.js
+++ gruntfile.js
@@ -11,7 +11,8 @@ module.exports = function(grunt) {
 		bower: {
 			js: {
 				bootstrap: 'bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-				jquery: 'bower_components/jquery/dist/jquery.min.js'
+				jquery: 'bower_components/jquery/dist/jquery.min.js',
+                moment: 'bower_components/moment/min/moment.min.js'
 			}
 		},
 
@@ -21,7 +22,8 @@ module.exports = function(grunt) {
 			assets: { src: ['assets/*', '!assets/favicon.ico'], dest:'<%= dirs.output %>/assets/', expand: true, flatten: true },
 			favicon: { src: 'assets/favicon.ico', dest: '<%= dirs.output %>/', expand: true, flatten: true },
 			bootstrap: { src: '<%= bower.js.bootstrap %>', dest: '<%= dirs.output %>/js/vendor/', expand: true, flatten: true },
-			jquery: { src: '<%= bower.js.jquery %>', dest: '<%= dirs.output %>/js/vendor/', expand: true, flatten: true }
+			jquery: { src: '<%= bower.js.jquery %>', dest: '<%= dirs.output %>/js/vendor/', expand: true, flatten: true },
+            moment: { src: '<%= bower.js.moment %>', dest: '<%= dirs.output %>/js/vendor/', expand: true, flatten: true }
 		},
 		jade: {
 			build:{
@@ -56,23 +58,7 @@ module.exports = function(grunt) {
 				},
 				files: { '<%= dirs.output %>/js/main.js': 'scripts/*.coffee' }
 			}
-		},
-
-        // Wintersmith build tasks
-        metalsmith: {
-            blog:{
-                options: {
-                    metadata: {
-                        name: "<%= pkg.name %>",
-                        desc: "<%= pkg.description %>",
-                        author: "<%= pkg.author %>"
-                    },
-                    plugins: grunt.file.readJSON('metalsmith.json')
-                },
-                src: '<%= dirs.sources %>',
-                dest: '<%= dirs.output %>/blog'
-            }
-        }
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-clean');
@@ -80,7 +66,23 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jade');
     grunt.loadNpmTasks('grunt-contrib-coffee');
     grunt.loadNpmTasks('grunt-sass');
+
+    // Metalsmith blog task
     grunt.loadNpmTasks('grunt-metalsmith');
+    grunt.config('metalsmith', {
+        blog:{
+            options: {
+                metadata: {
+                    name: "<%= pkg.name %>",
+                    desc: "<%= pkg.description %>",
+                    author: "<%= pkg.author %>"
+                },
+                plugins: grunt.file.readJSON('metalsmith.json')
+            },
+            src: '<%= dirs.sources %>',
+            dest: '<%= dirs.output %>/blog'
+        }
+    });
 
     grunt.loadNpmTasks('grunt-responsive-images');
     grunt.config('responsive_images', {
@@ -93,7 +95,12 @@ module.exports = function(grunt) {
                 { name: "lg", width: 1200 }
             ]
         },
-        assets: { src: 'assets/*.{gif,png,jpg,jpeg}', dest:'<%= dirs.output %>/assets/', expand: true, flatten: true }
+        assets: {
+            src: 'assets/*.{gif,png,jpg,jpeg}',
+            dest:'<%= dirs.output %>/assets/',
+            expand: true,
+            flatten: true
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
@@ -106,20 +113,28 @@ module.exports = function(grunt) {
             tasks: ['jade']
         },
         sass: {
-            files: '**/*.scss',
+            files: 'styles/*.scss',
             tasks: ['sass']
         },
         coffee: {
-            files: '**/*.coffee',
+            files: 'scripts/*.coffee',
             tasks: ['coffee']
         },
         metalsmith: {
-            files: ['metalsmith.json', 'templates/layout.jade', 'templates/blog.jade', 'content/**/*'],
+            files: ['metalsmith.json', 'templates/layout.jade', 'templates/blog.jade', 'templates/article.jade', 'content/**/*'],
             tasks: ['metalsmith']
         }
     });
 
+    grunt.loadNpmTasks('grunt-sitemap');
+    grunt.config('sitemap', {
+        site: {
+            pattern: '<%= dirs.output %>/**/*.html',
+            siteRoot: '<%= dirs.output %>'
+        }
+    });
+
 	grunt.registerTask('build', 'Compiles all of the source files and copies the assets to the build directory.', ['clean', 'copy', 'responsive_images', 'coffee', 'sass', 'jade']);
-    grunt.registerTask('build:all', 'Compiles all of the source files (blog included)', ['build', 'metalsmith']);
+    grunt.registerTask('build:all', 'Compiles all of the source files (blog included)', ['build', 'metalsmith', 'sitemap']);
 	grunt.registerTask('default', 'Compiles everything and copies it to build directory.', ['build']);
 };
